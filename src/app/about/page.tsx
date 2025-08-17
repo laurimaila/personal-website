44a436a0eac2279a01ed23d126498ac716689461
Lauri Maila <laurikmaila@gmail.com>
2025-08-18 01:41:52 +0300

    Moved Flux manifests to a separate repo, small change to About-page bump:1.0.2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 
 const markdown = `# About Me
 
-I'm an Information and Communication Technology MSc (Tech) student at the University of Turku, majoring in Software Engineering with a minor in Data Analytics.
+I'm an ICT MSc (Tech) student at the University of Turku, majoring in Software Engineering with a minor in Data Analytics.
 
 Currently focusing on a mix of full-stack development, data automation and distributed systems. I also have some experience in embedded systems and scientific computing.`;
 
