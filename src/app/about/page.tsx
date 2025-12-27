1253b0ff7d05aa1704ecea9ec26972139fe0de67
Lauri Maila <laurikmaila@gmail.com>
2025-12-27 23:08:00 +0200

    Updated deps and CI to use node 24, bump:1.2.3

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,9 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 
 const markdown = `# About Me
 
-I'm an ICT MSc (Tech) student at the University of Turku, majoring in Software Engineering with a minor in Data Analytics.
+I'm an ICT MSc (Tech) student at the University of Turku, majoring in software engineering with a minor in data analytics.
 
-Currently focusing on a mix of full-stack development, data automation and distributed systems. I also have some experience in embedded systems and scientific computing.`;
+Currently focusing on a mix of full-stack development, data automation and cloud architecture. I also have some experience in embedded systems and scientific computing.`;
 
 export async function generateMetadata() {
     return {
